dfdc080c53ce8ae73d23c35933902a5a9b643888
Patel-Harshil <140731976+Patel-Harshil@users.noreply.github.com>
2023-07-27 23:04:07 +0530

    My Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:my_flutter_app/firebase_options.dart';
+// import 'package:my_flutter_app/views/login_view.dart';
+// import 'package:my_flutter_app/views/register_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false)
+                print("You're a verified user");
+              else
+                print("You need to verify your email-ID");
+              return const Text('Done');
+            default:
+              return const Text("Loading...");
+          }
+        }, //Builder
+      ),
+    );
+  }
+}
