73c8f64236278d8609adaae52a47b836501946f4
Patel-Harshil <140731976+Patel-Harshil@users.noreply.github.com>
2023-08-06 00:18:36 +0530

    steo-14 New note box

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -23,10 +23,13 @@ class _NotesViewState extends State<NotesView> {
   }
 
   @override
-  void dispose() {
+  @override
+@override
+void dispose() {
     _notesService.close();
-    super.dispose();
-  }
+  super.dispose();
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -78,6 +81,7 @@ class _NotesViewState extends State<NotesView> {
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
+                    case ConnectionState.active:
                       return const Text("Waiting for all notes...");
                     default:
                       return const CircularProgressIndicator();
